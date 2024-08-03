db2d623dc95c660fecd30afc41745d5dd75c73bb
YuanziX <75538551+YuanziX@users.noreply.github.com>
2024-08-03 16:42:07 +0530

    Time: 3 ms (84.69%) | Memory: 49.8 MB (30.97%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,29 @@
+class Solution {
+    void markAllAdjacentOnes(int row, int col, char[][] grid, int m, int n) {
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        markAllAdjacentOnes(row - 1, col, grid, m, n);
+        markAllAdjacentOnes(row + 1, col, grid, m, n);
+        markAllAdjacentOnes(row, col - 1, grid, m, n);
+        markAllAdjacentOnes(row, col + 1, grid, m, n);
+    }
+
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int islandsCount = 0;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == '1') {
+                    markAllAdjacentOnes(row, col, grid, m, n);
+                    islandsCount++;
+                }
+            }
+        }
+
+        return islandsCount;
+    }
+}
