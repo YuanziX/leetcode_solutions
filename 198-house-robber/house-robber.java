8c4ed1433609d47f790d9027cc71089eb0a30b3c
YuanziX <75538551+YuanziX@users.noreply.github.com>
2024-08-07 16:45:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (7.78%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] memo;
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int prev = 0;
+        int beforePrev = 0;
+
+        for (int num: nums) {
+            int tmp = prev;
+            prev = Math.max(beforePrev + num, prev);
+            beforePrev = tmp;
+        }
+        return prev;
+    }
+}
