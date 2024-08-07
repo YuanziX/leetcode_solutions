580e1ac86a099147b16160c8fb6d0a56ef3b2047
YuanziX <75538551+YuanziX@users.noreply.github.com>
2024-08-07 15:40:36 +0530

    Time: 1 ms (99.87%) | Memory: 46.2 MB (30.13%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,12 @@
 import java.util.Arrays;
 class Solution {
     public static int singleNumber(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+        int num = 0;
+
+        for (int n: nums) {
+            num = n ^ num;
         }
 
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 1; i+=2) {
-            if (nums[i] != nums[i+1]) {
-                return nums[i];
-            }
-        }
-        return nums[nums.length - 1];
+        return num;
     }
 }
