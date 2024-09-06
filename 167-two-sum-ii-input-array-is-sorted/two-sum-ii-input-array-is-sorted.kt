e4c5d457a0f1af3a19c77d184e7c6d21a6c716e0
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2024-09-07 00:34:54 +0530

    Time: 194 ms (96.86%) | Memory: 39.9 MB (50.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.kt
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.kt
@@ -1,16 +1,19 @@
 
 class Solution {
-    fun twoSum(numbers: IntArray, target: Int): IntArray? {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
         var l = 0
         var r = numbers.size - 1
         while (l < r) {
-            val sum = numbers[l] + numbers[r]
-            when {
-                sum < target -> l++
-                sum > target -> r--
-                else -> return intArrayOf(l + 1, r + 1)
+            val sum = 
+            if (numbers[l] + numbers[r] < target) {
+                l++
+            } else if (numbers[l] + numbers[r] > target) {
+                r--
+            }
+            else {
+                return intArrayOf(l + 1, r + 1)
             }
         }
-        return null
+        return intArrayOf()
     }
 }
