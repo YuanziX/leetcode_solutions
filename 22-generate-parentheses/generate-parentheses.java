f83290a7c8743275baa52c98eca8799e1de2992f
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2024-09-17 20:19:38 +0530

    Time: 1 ms (86.85%) | Memory: 43.4 MB (23.82%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,26 +1,22 @@
 class Solution {
-    private void recursive(int n, String dat, int numOpen, int numClosed, List<String> res) {
+    private void recursive(int n, StringBuilder dat, int numOpen, int numClosed, List<String> res) {
         if (dat.length() == 2*n) {
-            res.add(dat);
+            res.add(dat.toString());
             return;
         }
 
         if (numOpen < n) {
-            recursive(n, dat + "(", numOpen+1, numClosed, res);
+            recursive(n, new StringBuilder(dat).append("("), numOpen+1, numClosed, res);
         }
 
         if (numOpen > numClosed) {
-            recursive(n, dat + ")", numOpen, numClosed+1, res);
+            recursive(n, new StringBuilder(dat).append(")"), numOpen, numClosed+1, res);
         }
-    }
-
-    public void recurse(int n, List<String> res) {
-        recursive(n, "(", 1, 0, res);
     }
 
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        recurse(n, res);
+        recursive(n, new StringBuilder("("), 1, 0, res);
         return res;
     }
 }
