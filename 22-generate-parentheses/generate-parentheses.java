1e246fbfb9a3efecfc8eafd17a208c85e5e63230
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2024-09-17 20:11:09 +0530

    Time: 2 ms (54.67%) | Memory: 43.6 MB (18.81%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,72 +1,26 @@
-class CurrentBracketState {
-    String data = "";
-    int numOpen;
-    int numClosed;
-
-    public CurrentBracketState(String state, int o, int c) {
-        data = state;
-        numOpen = o;
-        numClosed = c;
-    }
-}
-
-class Tree {
-    TreeNode root;
-
-    public Tree() {
-        this.root = new TreeNode(new CurrentBracketState("(", 1, 0));
-    }
-
-    public void collectLeaf(TreeNode node, List<String> result) {
-        if (node == null) {
+class Solution {
+    private void recursive(int n, String dat, int numOpen, int numClosed, List<String> res) {
+        if (dat.length() == 2*n) {
+            res.add(dat);
             return;
         }
 
-        if (node.left == null && node.right == null) {
-            result.add(node.val.data);
+        if (numOpen < n) {
+            recursive(n, dat + "(", numOpen+1, numClosed, res);
         }
 
-        collectLeaf(node.left, result);
-        collectLeaf(node.right, result);
+        if (numOpen > numClosed) {
+            recursive(n, dat + ")", numOpen, numClosed+1, res);
+        }
     }
 
-}
-
-class TreeNode {
-    CurrentBracketState val;
-    TreeNode left;
-    TreeNode right;
-
-    public TreeNode(CurrentBracketState val) {
-        this.val = val;
+    public void recurse(int n, List<String> res) {
+        recursive(n, "(", 1, 0, res);
     }
-}
 
-public class Solution {
     public List<String> generateParenthesis(int n) {
-        Tree t = new Tree();
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(t.root);
-
-        while (!q.isEmpty()) {
-            TreeNode temp = q.poll();
-
-            if (temp.val.numOpen < n) {
-                TreeNode left = new TreeNode(
-                        new CurrentBracketState(temp.val.data + "(", temp.val.numOpen + 1, temp.val.numClosed));
-                temp.left = left;
-                q.add(left);
-            }
-            TreeNode right = new TreeNode(
-                    new CurrentBracketState(temp.val.data + ")", temp.val.numOpen, temp.val.numClosed + 1));
-            if (temp.val.numOpen > temp.val.numClosed) {
-                temp.right = right;
-                q.add(right);
-            }
-        }
-
-        List<String> result = new ArrayList<String>();
-        t.collectLeaf(t.root, result);
-        return result;
+        List<String> res = new ArrayList<>();
+        recurse(n, res);
+        return res;
     }
 }
