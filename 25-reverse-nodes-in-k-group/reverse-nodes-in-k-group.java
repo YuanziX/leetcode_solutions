9d1df30e1359135662550b1097727dd23cae6d4f
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-02-28 12:11:29 +0530

    Time: 1 ms (36.69%) | Memory: 44.3 MB (72.85%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -15,15 +15,19 @@ class Solution {
             return head;
         }
 
-        ListNode current = head;
-
         ListNode dummy = new ListNode(-1);
         ListNode tail = dummy;
+        ListNode current = head;
 
-        for (int i = l; i >= k; i -= k) {
-            ListNode firstNode = current;
+        // iterate for n k-groups
+        for (int i = 0; i < l / k; i++) {
+            // keep pointer on first node, this will be reversed so it'll be at the end
+            // 1 -> 2
+            // ^
+            ListNode first = current;
             ListNode before = null, after;
 
+            // revert jth k-group
             for (int j = 0; j < k; j++) {
                 after = current.next;
                 current.next = before;
@@ -31,10 +35,16 @@ class Solution {
                 current = after;
             }
 
+            // attach reversed k-group to dummy node
             tail.next = before;
-            tail = firstNode;
+            // move tail to end of the reversed k group which is the actual new tail
+            // reversed = 2 -> 1
+            //                 ^ 'first' pointer
+            tail = first;
         }
 
+        // attach remaining nodes
+        // left over from non k-group (li < k)
         tail.next = current;
 
         return dummy.next;
