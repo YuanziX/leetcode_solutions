8c1b7b030ab4d213fac4b9b65dad1588d6a9e2ca
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-01 10:33:58 +0530

    Time: 2 ms (97.73%) | Memory: 41.6 MB (91.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-
         Stack<Character> st = new Stack<>();
 
         for (char c: s.toCharArray()) {
