d9acf089db5f2c2ff6da6828de41e1db961aecc1
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-04 01:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (6.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,16 @@
 class Solution {
     public int fib(int n) {
-        System.gc();
         if (n < 2) return n;
-        int a = 0, b = 1;
+        System.gc();
 
-        for (int i = 0; i < n; i++) {
-            int temp = a;
-            a = b;
-            b += temp;
+        Integer[] memo = new Integer[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
         }
 
-        return a;
+        return memo[n];
     }
 }
