a41c6826d818ee3159194cf99701f0336f335ba0
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-04 00:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (34.03%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,17 +15,34 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        traverse(root);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        TreeNode temp = null;
+
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                temp = stack.peek().right;
+
+                if (temp == null) {
+                    temp = stack.pop();
+                    invert(temp);
+                    while (!stack.isEmpty() && stack.peek().right == temp) {
+                        temp = stack.pop();
+                        invert(temp);
+                    }
+                } else {
+                    node = temp;
+                }
+            }
+        }
+
         return root;
     }
 
-    private void traverse(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-
-        traverse(node.left);
-        traverse(node.right);
+    private void invert(TreeNode node) {
         TreeNode temp = node.left;
         node.left = node.right;
         node.right = temp;
