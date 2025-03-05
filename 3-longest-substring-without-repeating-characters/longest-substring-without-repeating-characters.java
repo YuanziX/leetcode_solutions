1da7fb4233e09051f31f6dafeab184626db47908
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-05 12:01:28 +0530

    Time: 7 ms (37.96%) | Memory: 44.9 MB (31.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> res = new HashSet<>();
+        Set<Character> res = new HashSet<>(128);
         int max = 0;
         int i = 0, j = 0;
 
