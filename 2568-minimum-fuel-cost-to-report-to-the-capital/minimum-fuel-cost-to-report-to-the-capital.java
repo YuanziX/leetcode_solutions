57ec03189f4485b4974fd44921af6d088d56b54c
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-05 10:29:02 +0530

    Time: 19 ms (99.74%) | Memory: 104.2 MB (42.82%) - LeetSync

--- 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.java
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.java
@@ -1,35 +1,47 @@
 class Solution {
+    static int n;
+    static int[] head = new int[200002];
+    static int[] next = new int[200002];
+    static int[] to = new int[200002];
+    static long[] cost = new long[200002];
+    static int[] size = new int[200002];
+    static int cnt;
+
     public long minimumFuelCost(int[][] roads, int seats) {
-        int n = roads.length + 1;
+        n = roads.length + 1;
+        build();
+        for (int[] road : roads) {
+            next[cnt] = head[road[0]];
+            to[cnt] = road[1];
+            head[road[0]] = cnt++;
 
-        List<List<Integer>> cities = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            cities.add(new ArrayList<>());
+            next[cnt] = head[road[1]];
+            to[cnt] = road[0];
+            head[road[1]] = cnt++;
         }
-
-        for (int[] r: roads) {
-            cities.get(r[0]).add(r[1]);
-            cities.get(r[1]).add(r[0]);
-        }
-
-        return helper(0, -1, cities, seats)[1];
+        f(0, -1, seats);
+        return cost[0];
     }
 
-    public long[] helper(int curr, int parent, List<List<Integer>> cities, int seats) {
-        long numPeople = curr == 0 ? 0 : 1;
-        long fuel = 0;
-
-        for (int c: cities.get(curr)) {
-            if (c != parent) {
-                long[] sub = helper(c, curr, cities, seats);
-                numPeople += sub[0];
-                fuel += sub[1];
+    void f(int curr, int prev, int seats) {
+        for (int i = head[curr]; i > 0; i = next[i]) {
+            if (to[i] != prev) {
+                f(to[i], curr, seats);
+                size[curr] += size[to[i]];
+                cost[curr] += cost[to[i]];
+                cost[curr] += (size[to[i]] + seats - 1) / seats;
             }
         }
+    }
 
-        int cars = (int) Math.ceil(numPeople / (double) seats);
-        if (curr != 0) fuel += cars;
-        return new long[] {numPeople, fuel};
+    private void build() {
+        cnt = 1;
+        for (int i = 0; i < n; i++) {
+            head[i] = 0;
+            next[i] = 0;
+            to[i] = 0;
+            cost[i] = 0;
+            size[i] = 1;
+        }
     }
 }
