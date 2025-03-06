9a92b3ea78a9ef47d858717cc3827a26dbe86c96
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-06 10:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (78.31%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -5,20 +5,22 @@ class Solution {
         List<String> res = new ArrayList<>();
         if (digits.length() == 0) return res;
 
-        helper(digits, "", 0, res);
+        helper(digits, new StringBuilder(), 0, res);
         return res;
     }
 
-    public void helper(String digits, String curr, int pos, List<String> res) {
+    public void helper(String digits, StringBuilder curr, int pos, List<String> res) {
         if (pos == digits.length()) {
-            res.add(curr);
+            res.add(curr.toString());
             return;
         }
 
         String combo = map[Character.getNumericValue(digits.charAt(pos))];
 
         for (char c: combo.toCharArray()) {
-            helper(digits, curr + c, pos + 1, res);
+            curr.append(c);
+            helper(digits, curr, pos + 1, res);
+            curr.setLength(curr.length() - 1);
         }
     }
 }
