ed768e9f356798743d23e3d8cdb188034b29664d
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-14 20:41:49 +0530

    Time: 112 ms (24.91%) | Memory: 57.5 MB (62.72%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,48 +1,49 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] pse = new int[heights.length];
-        int[] nse = new int[heights.length];
+        int n = heights.length;
+        int[] ls = new int[n];
+        int[] rs = new int[n];
 
         Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i < heights.length; i++) {
-            while (!st.empty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-
-            if (st.empty()) {
-                pse[i] = i + 1;
-            } else {
-                pse[i] = i - st.peek();
-            }
-
-            st.push(i);
-        }
-
-        st.clear();
-
-        for (int i = heights.length - 1; i >= 0; i--) {
+        for (int i = n - 1; i > -1; i--) {
             while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
                 st.pop();
             }
 
-            if (st.isEmpty()) {
-                nse[i] = heights.length - i;
+            if (!st.isEmpty()) {
+                rs[i] = st.peek() - i;
             } else {
-                nse[i] = st.peek() - i;
+                rs[i] = n - i;
             }
+
             st.push(i);
         }
 
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < heights.length; i++) {
-            int calc = heights[i] * (nse[i] + pse[i] - 1);
-            if (calc > max) {
-                max = calc;
-            }
+        while (!st.isEmpty()) {
+            st.pop();
         }
 
-        return max;
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (!st.isEmpty()) {
+                ls[i] = i - st.peek();
+            } else {
+                ls[i] = i + 1;
+            }
+
+            st.push(i);
+        }
+
+        int maxHeight = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            maxHeight = Math.max(maxHeight, heights[i] * (ls[i] + rs[i] - 1));
+        }
+
+        return maxHeight;
     }
 }
