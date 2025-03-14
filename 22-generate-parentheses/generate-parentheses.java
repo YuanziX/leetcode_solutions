0872c0c70e05d3127a30528657e7b986ea822e8e
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-14 14:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (89.02%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        helper(n, new StringBuilder(), 0, 0, res);
+        if (n == 0) return res;
+        helper(new StringBuilder(), n, 0, 0, res);
         return res;
     }
 
-    public void helper(int n, StringBuilder curr, int l, int r, List<String> res) {
-        if (l + r == 2 * n) {
-            res.add(curr.toString());
+    private void helper(StringBuilder cur, int n, int l, int r, List<String> res) {
+        if (cur.length() == n * 2) {
+            res.add(cur.toString());
             return;
         }
 
         if (l < n) {
-            curr.append('(');
-            helper(n, curr, l + 1, r, res);
-            curr.setLength(curr.length() - 1);
+            cur.append('(');
+            helper(cur, n, l + 1, r, res);
+            cur.setLength(cur.length() - 1);
         }
 
         if (r < l) {
-        curr.append(')');
-            helper(n, curr, l, r + 1, res);
-            curr.setLength(curr.length() - 1);
+            cur.append(')');
+            helper(cur, n, l, r + 1, res);
+            cur.setLength(cur.length() - 1);
         }
     }
 }
