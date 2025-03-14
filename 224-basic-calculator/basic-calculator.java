fcd773990cddf804ea7302bddbf51bc321d9ccfd
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-15 01:28:05 +0530

    Time: 7 ms (83.34%) | Memory: 45 MB (22.94%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,35 +1,34 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> st = new Stack<>();
-        int number = 0;
         int res = 0;
+        int num = 0;
         int sign = 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (char c: s.toCharArray()) {
             if (Character.isDigit(c)) {
-                number = 10 * number + (c - '0');
+                num = num * 10 + (c - '0');
             } else if (c == '+') {
-                res = res + sign * number;
-                number = 0;
+                res += sign * num;
                 sign = 1;
+                num = 0;
             } else if (c == '-') {
-                res = res + sign * number;
-                number = 0;
+                res += sign * num;
                 sign = -1;
+                num = 0;
             } else if (c == '(') {
                 st.push(res);
                 st.push(sign);
-                sign = 1;
                 res = 0;
+                sign = 1;
             } else if (c == ')') {
-                res += sign * number;
-                number = 0;
+                res += num * sign;
+                num = 0;
                 res *= st.pop();
                 res += st.pop();
             }
         }
 
-        return res + sign * number;
+        return res + sign * num;
     }
 }
