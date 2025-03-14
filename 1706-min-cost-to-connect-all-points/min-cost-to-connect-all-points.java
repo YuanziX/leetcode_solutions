13043290275e8df3aa85e7fe20a75b86388206c2
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-14 20:26:58 +0530

    Time: 1343 ms (5.02%) | Memory: 77.3 MB (31.03%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,71 +1,30 @@
 class Solution {
-    static class Edge implements Comparable<Edge> {
-        int s, d, w;
-        public Edge(int s, int d, int w) {
-            this.s = s;
-            this.d = d;
-            this.w = w;
-        }
-
-        public int compareTo(Edge o) {
-            return this.w - o.w;
-        }
-    }
-
-    static class DisjointSet {
-        int[] rank;
-        int[] parent;
-
-        public DisjointSet(int v) {
-            rank = new int[v];
-            parent = new int[v];
-
-            for (int i = 0; i < v; i++) {
-                rank[i] = 0;
-                parent[i] = i;
-            }
-        }
-
-        int findParent(int v) {
-            if (parent[v] != v) {
-                return findParent(parent[v]);
-            }
-            return parent[v];
-        }
-
-        void union(int x, int y) {
-            int xp = findParent(x);
-            int yp = findParent(y);
-
-            if (rank[xp] > rank[yp]) {
-                parent[yp] = xp;
-            } else if (rank[yp] > rank[xp]) {
-                parent[xp] = yp;
-            } else {
-                parent[yp] = xp;
-                rank[xp]++;
-            }
-        }
-    }
 
     public int minCostConnectPoints(int[][] points) {
-        List<Edge> edges = new ArrayList<>();
-        for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                edges.add(new Edge(i, j, Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1])));
+        boolean[] vis = new boolean[points.length];
+        // dist, vertex
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        pq.offer(new int[] {0, 0});
+        int cost = 0;
+        while (!pq.isEmpty()) {
+            int[] next = pq.poll();
+            int c = next[0];
+            int v = next[1];
+
+            if (vis[v]) {
+                continue;
+            }
+
+            cost += c;
+            vis[v] = true;
+
+            for (int i = 0; i < points.length; i++) {
+                int dist = Math.abs(points[i][0] - points[v][0]) + Math.abs(points[i][1] - points[v][1]);
+                pq.add(new int[]{dist, i});
             }
         }
 
-        Collections.sort(edges);
-        int mc = 0;
-        DisjointSet ds = new DisjointSet(points.length);
-        for (Edge e: edges) {
-            if (ds.findParent(e.s) != ds.findParent(e.d)) {
-                ds.union(e.s, e.d);
-                mc += e.w;
-            }
-        }
-
-        return mc;
+        return cost;
     }
 }
