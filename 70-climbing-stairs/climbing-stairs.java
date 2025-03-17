388729fd553853adbca57a0bfb9f4946d9ed7fc3
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-17 14:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (27.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,9 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
+        // int[] memo = new int[n + 1];
 
-        Arrays.fill(memo, -1);
-        return helper(n, 0, memo);
+        // Arrays.fill(memo, -1);
+
+        // for (int i = 0; i < n; i++) {
+            
+        // }
+
+        return tabulation(n);
+    }
+
+    private int tabulation(int n) {
+
+        int[] memo = new int[n + 1];
+        memo[n] = 1;
+        memo[n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            int n1 = memo[i + 1], n2 = memo[i + 2];
+            memo[i] = n1 + n2;
+        }
+        return memo[0];
     }
 
     public int helper(int n, int i, int[] memo) {
@@ -11,22 +29,18 @@ class Solution {
         if (i > n) return 0;
 
         int n1 = 0, n2 = 0;
-        if (i + 1 <= n) {
-            if (memo[i] != -1) {
-                n1 = memo[i];
-            } else {
-                n1 = helper(n, i + 1, memo);
-                memo[i] = n1;
-            }
+        if (memo[i] != -1) {
+            n1 = memo[i];
+        } else {
+            n1 = helper(n, i + 1, memo);
+            memo[i] = n1;
         }
 
-        if (i + 2 <= n) {
-            if (memo[i + 1] != -1) {
-                n2 = memo[i + 1];
-            } else {
-                n2 = helper(n, i + 2, memo);
-                memo[i + 1] = n2;
-            }
+        if (memo[i + 1] != -1) {
+            n2 = memo[i + 1];
+        } else {
+            n2 = helper(n, i + 2, memo);
+            memo[i + 1] = n2;
         }
 
         return n1 + n2;
