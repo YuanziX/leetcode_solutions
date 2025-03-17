39bdee1acc98b3be20d3e3642f262ba3b5b52c5e
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-17 14:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (49.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,34 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
-        int[][] memo = new int[n][2];
+        int[] memo = new int[n + 1];
 
-        for (int[] i: memo) {
-            Arrays.fill(i, -1);
-        }
-
+        Arrays.fill(memo, -1);
         return helper(n, 0, memo);
     }
 
-    public int helper(int n, int i, int[][] memo) {
+    public int helper(int n, int i, int[] memo) {
         if (i == n) return 1;
         if (i > n) return 0;
 
         int n1 = 0, n2 = 0;
         if (i + 1 <= n) {
-            if (memo[i][0] != -1) {
-                n1 = memo[i][0];
+            if (memo[i] != -1) {
+                n1 = memo[i];
             } else {
                 n1 = helper(n, i + 1, memo);
-                memo[i][0] = n1;
+                memo[i] = n1;
             }
         }
 
         if (i + 2 <= n) {
-            if (memo[i][1] != -1) {
-                n2 = memo[i][1];
+            if (memo[i + 1] != -1) {
+                n2 = memo[i + 1];
             } else {
                 n2 = helper(n, i + 2, memo);
-                memo[i][1] = n2;
+                memo[i + 1] = n2;
             }
         }
 
