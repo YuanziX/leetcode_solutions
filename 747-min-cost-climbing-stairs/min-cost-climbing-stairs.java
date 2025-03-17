5435e227e8e9dce94e24c0e01734197b6b40ddb8
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-17 15:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (76.77%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -4,21 +4,27 @@ class Solution {
     }
 
 	public int tabulation(int[] cost) {
-		int n = cost.length;
-		int[] dp = new int[n];
+        if (cost.length == 2) return Math.min(cost[0], cost[1]);
 
-		dp[n - 1] = cost[n - 1];
-		dp[n - 2] = cost[n - 2];
+		int n = cost.length;
+
+		int i2 = cost[n - 1], i1 = cost[n - 2];
 
 		for (int i = n - 3; i >= 1; i--) {
-			dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+			int t = i1;
+			i1 = cost[i] + Math.min(i1, i2);
+			i2 = t;
 		}
-		int c1 = dp[1];
+		int c1 = i1;
+		i2 = cost[n - 1];
+		i1 = cost[n - 2];
 
 		for (int i = n - 3; i >= 0; i--) {
-			dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+			int t = i1;
+			i1 = cost[i] + Math.min(i1, i2);
+			i2 = t;
 		}
 
-		return Math.min(dp[0], c1);
+		return Math.min(c1, i1);
 	}
 }
