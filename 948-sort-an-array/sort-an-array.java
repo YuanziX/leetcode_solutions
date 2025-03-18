e236e852ecbaf4aa0d2cee67324429b0976fe678
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-18 19:38:52 +0530

    Time: 1529 ms (5.63%) | Memory: 59.7 MB (5.12%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,37 @@
 class Solution {
+    private void swap(int[] nums, int l, int r) {
+        int t = nums[r];
+        nums[r] = nums[l];
+        nums[l] = t;
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int piv = nums[l + (r - l) / 2];
+        int i = l - 1;
+        swap(nums, r, l + (r - l) / 2);
+
+        for (int j = l; j < r; j++) {
+            if (nums[j] < piv) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+
+        swap(nums, i + 1, r);
+        return i + 1;
+    }
+
+    private void qs(int[] nums, int l, int r) {
+        if (l < r) {
+            int p = partition(nums, l, r);
+
+            qs(nums, l, p - 1);
+            qs(nums, p + 1, r);
+        }
+    }
+
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        qs(nums, 0, nums.length - 1);
         return nums;
     }
 }
