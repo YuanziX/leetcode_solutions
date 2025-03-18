651982ff86659cf24cdf38225408159f1f81eaae
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-18 19:40:48 +0530

    Time: 90 ms (15.38%) | Memory: 56.8 MB (27.97%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,37 +1,15 @@
 class Solution {
-    private void swap(int[] nums, int l, int r) {
-        int t = nums[r];
-        nums[r] = nums[l];
-        nums[l] = t;
-    }
-
-    private int partition(int[] nums, int l, int r) {
-        int piv = nums[l + (r - l) / 2];
-        int i = l - 1;
-        swap(nums, r, l + (r - l) / 2);
-
-        for (int j = l; j < r; j++) {
-            if (nums[j] < piv) {
-                i++;
-                swap(nums, i, j);
-            }
-        }
-
-        swap(nums, i + 1, r);
-        return i + 1;
-    }
-
-    private void qs(int[] nums, int l, int r) {
-        if (l < r) {
-            int p = partition(nums, l, r);
-
-            qs(nums, l, p - 1);
-            qs(nums, p + 1, r);
-        }
-    }
-
     public int[] sortArray(int[] nums) {
-        qs(nums, 0, nums.length - 1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = pq.poll();
+        }
+
         return nums;
     }
 }
