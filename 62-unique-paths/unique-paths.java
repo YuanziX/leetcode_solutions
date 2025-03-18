73de9d38320628b978995283887e879d4c992613
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-18 18:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.02%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,34 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][][] memo = new int[m][n][2];
-        for (int[][] i: memo)  {
-            for (int[] j: i) {
-                Arrays.fill(j, -1);
-            }
+        int[][] memo = new int[m][n];
+        for (int[] j: memo) {
+            Arrays.fill(j, -1);
         }
 
         return helper(m, n, 0, 0, memo);
     }
 
-    int helper(int m, int n, int i, int j, int[][][] memo) {
+    int helper(int m, int n, int i, int j, int[][] memo) {
         if (i == m - 1 && j == n - 1) return 1;
         if (i >= m || j >= n) return 0;
 
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
         int n1 = 0, n2 = 0;
-        if (memo[i][j][0] != -1) {
-            n1 = memo[i][j][0];
-        } else {
-            n1 = helper(m, n, i + 1, j, memo);
-            memo[i][j][0] = n1;
-        }
+        n1 = helper(m, n, i + 1, j, memo);
+        n2 = helper(m, n, i, j + 1, memo);
 
-        if (memo[i][j][1] != -1) {
-            n2 = memo[i][j][1];
-        } else {
-            n2 = helper(m, n, i, j + 1, memo);
-            memo[i][j][1] = n2;
-        }
-
-        return n1 + n2;
+        return memo[i][j] = n1 + n2;
     }
 }
