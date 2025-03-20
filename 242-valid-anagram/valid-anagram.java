bb9173c81a395668037b5577d1a867a4183544e9
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-20 17:49:21 +0530

    Time: 3 ms (90.16%) | Memory: 44.6 MB (45.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        PriorityQueue<Character> pq1 = new PriorityQueue<>();
-        PriorityQueue<Character> pq2 = new PriorityQueue<>();
-
-        for (char c: s.toCharArray()) {
-            pq1.add(c);
-        }
-        for (char c: t.toCharArray()) {
-            pq2.add(c);
-        }
-
-        while (!pq1.isEmpty() && !pq2.isEmpty() && pq1.peek().equals(pq2.peek())) {
-            pq1.poll();
-            pq2.poll();
-        }
-        return pq1.size() == 0 && 0 == pq2.size();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
     }
 }
