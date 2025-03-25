5ba29ab38085ed27bf5e4fcb57ea974da5ce4bba
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-25 19:29:22 +0530

    Time: 2 ms (84.14%) | Memory: 63.1 MB (19.02%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -3,13 +3,11 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < nums1.length && j < nums2.length) {
-            if (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) return nums1[i];
+            if (nums1[i] == nums2[j]) return nums1[i];
 
-            while (i < nums1.length && j < nums2.length && nums1[i] < nums2[j]) {
+            if (nums1[i] < nums2[j]) {
                 i++;
-            }
-
-            while (i < nums1.length && j < nums2.length && nums2[j] < nums1[i]) {
+            } else {
                 j++;
             }
         }
