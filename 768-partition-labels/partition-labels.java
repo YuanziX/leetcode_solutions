891a3b32ab3772fd175ea683a58778311566ec18
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-03-30 16:03:43 +0530

    Time: 9 ms (39.82%) | Memory: 42.8 MB (19.80%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,35 +1,21 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        Map<Character, Integer> end = new HashMap<>();
+        Map<Character, Integer> lastOccur = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            end.put(s.charAt(i), i);
-        }
-
-        List<int[]> intervals = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            intervals.add(new int[] { i, end.get(s.charAt(i)) });
-        }
-        Collections.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        List<int[]> merged = new ArrayList<>();
-        int[] prev = intervals.get(0);
-        merged.add(prev);
-
-        for (int i = 1; i < intervals.size(); i++) {
-            int[] curr = intervals.get(i);
-
-            if (curr[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], curr[1]);
-            } else {
-                merged.add(curr);
-                prev = curr;
-            }
+            lastOccur.put(s.charAt(i), i);
         }
 
         List<Integer> res = new ArrayList<>();
-        for (int[] i : merged) {
-            res.add(i[1] - i[0] + 1);
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccur.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(end - start + 1);
+                start = i + 1;
+            }
         }
 
         return res;
