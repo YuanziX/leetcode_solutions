306a578c81b4812c9f7d58fb4de0199fb007e018
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-04-13 04:21:51 +0530

    Time: 131 ms (86.36%) | Memory: 41.6 MB (78.20%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean res = false;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                res = helper(board, word, 0, i, j);
-                if (res) return res;
+                if (helper(board, word, 0, i, j)) return true;
             }
         }
 
-        return res;
+        return false;
     }
 
     boolean helper(char[][] board, String word, int i, int n, int m) {
