5f9f3171e99f4e85bd794bb13487882042829da3
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-04-13 03:07:22 +0530

    Time: 2 ms (39.30%) | Memory: 44.3 MB (96.80%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,22 +3,24 @@ class Solution {
         List<List<Integer>> res = new LinkedList<>();
         if (nums.length == 0) return res;
 
-        helper(nums, new LinkedHashSet<Integer>(), res);
+        boolean[] vis = new boolean[nums.length];
+        helper(nums, vis, new ArrayList<Integer>(), res);
         return res;
     }
 
-    private void helper(int[] nums, Set<Integer> sub, List<List<Integer>> res) {
+    private void helper(int[] nums, boolean[] vis, ArrayList<Integer> sub, List<List<Integer>> res) {
         if (sub.size() == nums.length) {
             res.add(new ArrayList<>(sub));
             return;
         }
 
-        for (int i : nums) {
-            if (!sub.contains(i)) {
-                sub.add(i);
-                helper(nums, sub, res);
-                sub.remove(i);
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (vis[i]) continue;
+            sub.add(nums[i]);
+            vis[i] = true;
+            helper(nums, vis, sub, res);
+            vis[i] = false;
+            sub.remove(sub.size() - 1);
         }
     }
 }
