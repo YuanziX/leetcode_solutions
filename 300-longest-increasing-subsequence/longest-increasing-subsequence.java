1fb345076a17bb79e6e5308b0abe389d71901922
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-04-22 21:15:13 +0530

    Time: 78 ms (22.35%) | Memory: 77.2 MB (17.51%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,37 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length + 1];
-        dp[0] = Integer.MIN_VALUE;
-        int ptr = 0;
+        int n = nums.length;
+        int[][] dp = new int[n + 1][n + 1];
 
-        for (int i = 0; i < nums.length; i++) {
-            if (dp[ptr] < nums[i]) {
-                ptr++;
-                dp[ptr] = nums[i];
-            } else {
-                dp[lowerBound(dp, ptr + 1, nums[i])] = nums[i];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+                int c1 = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    c1 = 1 + dp[i + 1][i + 1];
+                }
+                int c2 = dp[i + 1][prev + 1];
+                dp[i][prev + 1] = Math.max(c1, c2);
             }
         }
 
-        return ptr;
-    }
-
-    public static int lowerBound(int []arr, int n, int x) {
-        int ans = n;
-
-        int l = 0, r = n - 1;
-
-        while (l <= r) {
-            int m = (l + r) / 2;
-
-            if (arr[m] >= x) {
-                ans = m;
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
-        }
-
-        return ans;
+        return dp[0][0];
     }
 }
