8196ba8acc84f3cfed0730a5044967e43f49d002
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-07-12 11:25:45 +0530

    Time: 124 ms (7.29%) | Memory: 57.4 MB (58.33%) - LeetSync

---
+++ 3107-maximum-spending-after-buying-items/maximum-spending-after-buying-items.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maxSpending(int[][] values) {
+        int m = values.length, n = values[0].length;
+        long spending = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.add(values[i][j]);
+            }
+        }
+
+        int k = 1;
+        while (!pq.isEmpty()) {
+            spending += (double) pq.poll() * k++;
+        }
+
+        return spending;
+    }
+}
