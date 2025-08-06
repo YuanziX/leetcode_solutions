7fcf47683e0066423ae47a2f3020d3961ce3b31b
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-08-06 23:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (82.19%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -2,7 +2,8 @@ class Solution {
     public int minAddToMakeValid(String s) {
         int close = 0, open = 0;
 
-        for (char c: s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (c == '(') open++;
             else if (open > 0) open--;
             else close++;
