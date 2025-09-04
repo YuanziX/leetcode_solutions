4b0f599f1db8b9a79852cad7825cad765d953e27
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-09-04 12:17:41 +0530

    Time: 6 ms (75.82%) | Memory: 44.1 MB (92.17%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -3,17 +3,45 @@ import java.util.*;
 class Solution {
     public int[] frequencySort(int[] nums) {
         Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums)
+        for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
 
-        Integer[] arr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        quickSort(nums, 0, nums.length - 1, freq);
+        return nums;
+    }
 
-        Arrays.sort(arr, (a, b) -> {
-            int fa = freq.get(a), fb = freq.get(b);
-            if (fa == fb) return b - a;
-            return fa - fb;
-        });
+    private void quickSort(int[] nums, int left, int right, Map<Integer, Integer> freq) {
+        if (left >= right) return;
 
-        return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+        int pivotIndex = partition(nums, left, right, freq);
+        quickSort(nums, left, pivotIndex - 1, freq);
+        quickSort(nums, pivotIndex + 1, right, freq);
+    }
+
+    private int partition(int[] nums, int left, int right, Map<Integer, Integer> freq) {
+        int pivot = nums[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+            if (compare(nums[j], pivot, freq) < 0) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, right);
+        return i;
+    }
+
+    private int compare(int a, int b, Map<Integer, Integer> freq) {
+        int fa = freq.get(a), fb = freq.get(b);
+        if (fa != fb) return fa - fb;
+        return b - a;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
