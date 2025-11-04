7e37dab9aa2144c4d1575ed9316a315e8e5db325
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-11-05 00:35:20 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (12.58%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,31 +1,29 @@
 class Solution {
-    private Boolean[][] memo;
-
-    private boolean helper(String s, String p, int i, int j) {
+    public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
+        boolean[] dp = new boolean[m + 1];
+        boolean[] newDp;
 
-        if (memo[i][j] != null) return memo[i][j];
+        dp[m] = true;
 
-        boolean ans;
-
-        if (j == m) {
-            ans = (i == n);
-        } else {
-            boolean firstMatch = (i < n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
-
-            if (j + 1 < m && p.charAt(j + 1) == '*') {
-                ans = helper(s, p, i, j + 2) || (firstMatch && helper(s, p, i + 1, j));
-            } else {
-                ans = firstMatch && helper(s, p, i + 1, j + 1);
-            }
+        for (int j = m - 2; j >= 0; j -= 2) {
+            if (p.charAt(j + 1) == '*') dp[j] = dp[j + 2];
+            else break;
         }
 
-        memo[i][j] = ans;
-        return ans;
-    }
+        for (int i = n - 1; i >= 0; i--) {
+            newDp = new boolean[m + 1];
+            for (int j = m - 1; j >= 0; j--) {
+                boolean firstMatch = (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+                if (j + 1 < m && p.charAt(j + 1) == '*') {
+                    newDp[j] = newDp[j + 2] || (firstMatch && dp[j]);
+                } else {
+                    newDp[j] = firstMatch && dp[j + 1];
+                }
+            }
+            dp = newDp;
+        }
 
-    public boolean isMatch(String s, String p) {
-        memo = new Boolean[s.length() + 1][p.length() + 1];
-        return helper(s, p, 0, 0);
+        return dp[0];
     }
 }
