b1f326fb23f76202f6178fb4c2d722ffce35dd48
Achyut Badyal <75538551+YuanziX@users.noreply.github.com>
2025-12-09 03:18:17 +0530

    Time: 27 ms (30.71%) | Memory: 42.8 MB (15.15%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,13 +1,27 @@
 class Solution {
     public int countTriples(int n) {
-        int c = 0;
+        // Map perfectSquare -> sqrt(perfectSquare)
+        HashMap<Integer, Integer> sqrtMap = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            sqrtMap.put(i * i, i);
+        }
+
+        int count = 0;
+
         for (int a = 1; a <= n; a++) {
+            int a2 = a * a;
             for (int b = 1; b <= n; b++) {
-                int c2 = a * a + b * b;
-                int cRoot = (int)Math.sqrt(c2);
-                if (cRoot <= n && cRoot * cRoot == c2) c++;
+                int c2 = a2 + b * b;
+
+                // Check if c² exists in the map
+                Integer root = sqrtMap.get(c2);
+                if (root != null && root <= n) {
+                    count++;
+                }
             }
         }
-        return c;
+        System.gc();
+        return count;
     }
 }
